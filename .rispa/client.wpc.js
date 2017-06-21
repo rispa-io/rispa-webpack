1741b24c187c7dd2f00efa223b34463e19d7ff7c
dvpetrov <dvpetrov.main@gmail.com>
2017-06-21 10:32:51 +0300

    Clean before build

--- .rispa/client.wpc.js
+++ .rispa/client.wpc.js
@@ -1,8 +1,15 @@
+import path from 'path'
 import { createConfig, env } from '@webpack-blocks/webpack2'
+import CleanWebpackPlugin from 'clean-webpack-plugin'
+import config from '@rispa/config'
 
 export default () => createConfig([
   context => ({
     plugins: [
+      new CleanWebpackPlugin([config.outputPath.split(path.sep).pop()], {
+        exclude: ['activators.json'],
+        root: path.dirname(config.outputPath)
+      }),
       new context.webpack.optimize.CommonsChunkPlugin({
         names: ['bootstrap'],
         filename: '[name].js',
