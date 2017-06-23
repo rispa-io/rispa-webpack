302181fefb9af5e287239df9eeb54d83ed6fa52c
Maxim Samoilov <samoilowmaxim@gmail.com>
2017-06-23 10:34:49 +0300

    Move all javascript configuration into https://github.com/rispa-io/rispa-webpack-javascript

--- .rispa/activator.js
+++ .rispa/activator.js
@@ -1,16 +1,13 @@
 import { init, start, build } from '@rispa/core/events'
 import { server } from '@rispa/server/events'
 import runBuild from '../src/build'
-import getBabelOptions from './babel-options'
 import webpackExtensionCommon from './common.wpc'
 import webpackExtensionClient from './client.wpc'
 
 const activator = on => {
   const initHandler = registry => {
-    const webpackCommon = webpackExtensionCommon(registry)
-    registry.add('webpack.common', webpackCommon)
-    registry.add('webpack.client', webpackCommon, webpackExtensionClient)
-    registry.add('babel', getBabelOptions)
+    registry.add('webpack.common', webpackExtensionCommon)
+    registry.add('webpack.client', webpackExtensionCommon, webpackExtensionClient)
   }
 
   on(init(build), initHandler)
