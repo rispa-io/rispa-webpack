1fd88ac696ccc9fe8ebfe4ac5dbb4da860776905
Vladimir Ratsev <vratsev@gmail.com>
2017-06-26 11:00:28 +0500

    Add source map support

--- .rispa/client.wpc.js
+++ .rispa/client.wpc.js
@@ -5,6 +5,7 @@ import config from '@rispa/config'
 
 export default () => createConfig([
   context => ({
+    devtool: 'cheap-module-source-map',
     plugins: [
       new CleanWebpackPlugin([config.outputPath.split(path.sep).pop()], {
         exclude: ['activators.json'],
