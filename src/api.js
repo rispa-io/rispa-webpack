e222dfe38a1ef596b2fcddd800b6dba15504fd63
dvpetrov <dvpetrov.main@gmail.com>
2017-10-04 22:54:08 +0300

    Rewrite api to object

--- src/api.js
+++ src/api.js
@@ -1,11 +1,11 @@
-class WebpackPluginApi {
-  static startHandler(context) {
+const WebpackPluginApi = {
+  pluginName: '@rispa/webpack',
+
+  startHandler(context) {
     const instance = context.get(WebpackPluginApi.pluginName)
 
     return instance.runBuild()
-  }
+  },
 }
-
-WebpackPluginApi.pluginName = '@rispa/webpack'
 
 module.exports = WebpackPluginApi
